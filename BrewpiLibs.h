1ce2b51ccba12b9301f2ba065ee14f7779b51b2f
Kristian Wiklund <githubkristian@snabela.nl>
2021-05-14 16:21:57 +0200

    added headerfile to make arduino-cli happy

---
+++ BrewpiLibs.h
@@ -0,0 +1,11 @@
+#ifndef _BREWPILIBS_H
+#define _BREWPILIBS_H
+
+// this file mostly exists to make it possible to install directly from github using the arduino-cli tool
+
+#include <BrewpiBuzzer.h>
+#include <BrewpiLCD.h>
+#include <BrewpiPins.h>
+#include <BrewpiPrint.h>
+
+#endif
